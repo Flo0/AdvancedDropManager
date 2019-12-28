0c5cccef1396187c90d04ac889c90a242d2304f7
BuildTools <unconfigured@null.spigotmc.org>
2019-12-28 13:39:50 +0100

    Initial commit

---
+++ src/main/java/com/gestankbratwurst/advanceddropmanager/AdvancedDropManager.java
@@ -0,0 +1,17 @@
+package com.gestankbratwurst.advanceddropmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedDropManager extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
